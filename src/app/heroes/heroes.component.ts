592dd97b931d5cd2dfdb961fffcfef388284b794
Bella Wang <sunry81914@gmail.com>
2016-06-01 17:34:43 +0800

    feat: generate heroes component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
